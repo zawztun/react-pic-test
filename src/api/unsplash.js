3da71da01da04a531515676fa9b9e26b408b6ad2
Zaw Zaw Tun <zawzawtun@Zaws-MacBook-Pro.local>
2020-02-06 11:33:13 -0500

    change src folder to print

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,4 +1,4 @@
-/*import axios from 'axios';
+import axios from 'axios';
 
 export default axios.create({
     baseURL:'https://api.unsplash.com',
@@ -6,13 +6,5 @@ export default axios.create({
         Authorization: 'Client-ID 61cf7fcf49568571f63bdc2fd3e3f6369d98f30d683dabd2dfcf0cb92c7a3347'
     } 
 });
-*/
 
-import axios from 'axios';
 
-export default axios.create ({
-    baseURL:'https://api.unsplash.com',
-    headers:{
-        Authorization: 'Client-ID 61cf7fcf49568571f63bdc2fd3e3f6369d98f30d683dabd2dfcf0cb92c7a3347'
-    } 
-});
--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,4 @@
-import React from 'react';
-import unsplash from '../api/unsplash';
-import SearchBar from './SearchBar';
-import ImageList from './ImageList';
 
-class App extends React.Component{
-    state = {images: [] };
-
-     onSearchSubmit = async term => {
-      const response = await unsplash.get('/search/photos',{
-           params:{query:term},
-        
-            
-       });
-       this.setState( {images:response.data.results});
-    }
-
-    render(){
-    return (
-    <div className = "ui container" style = {{marginTop: '10px'}}>
-        <SearchBar onSubmit = {this.onSearchSubmit} />
-        <ImageList images= {this.state.images}/>
-    </div>
-    );
-    }
-}
-export default App;
-
-
-
-/*
-///////////////////
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
@@ -60,4 +29,3 @@ class App extends React.Component {
 }
 
 export default App;
-*/
